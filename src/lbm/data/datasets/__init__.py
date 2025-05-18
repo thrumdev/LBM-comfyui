ab3b1f1d1e0793b9312ec17389c7a9ef018afdcc
Robert Habermeier <rphmeier@gmail.com>
2025-05-18 12:19:49 -0500

    removemore unnecessary stuff + requirements

--- src/lbm/data/datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-A collection of :mod:`pytorch_lightning.LightningDataModule` used to train the models. In particular,
-they can be used to create the dataloaders and setup the data pipelines.
-"""
-
-from .dataset import DataModule
-from .datasets_config import DataModuleConfig
-
-__all__ = ["DataModule", "DataModuleConfig"]
